7243955f28b5758f52ba84e9c5fb10fb1222d3fd
wxb <480802338@qq.com>
2022-11-20 09:52:49 +0800

    first commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -3,5 +3,6 @@ package main.java.com.atguigu;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello boy!");
     }
 }
