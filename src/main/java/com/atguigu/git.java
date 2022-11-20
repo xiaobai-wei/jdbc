fb3030c4c7225074c5f2c42cc98603e63ad04fe7
wxb <480802338@qq.com>
2022-11-20 10:23:48 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hello git!");
         System.out.println("hello boy!");
         System.out.println("hello girl!");
+        System.out.println("hot-fix merge test!!!");
     }
 }
